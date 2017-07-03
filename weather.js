a557452131b46d022e7e447821b34f8e457becb0
Jiwon Baek <whitezonen1@gmail.com>
2017-07-03 05:14:08 +0000

    add time n imgs

--- weather.js
+++ weather.js
@@ -76,7 +76,6 @@ exports.realTimeWeather = function(xPos,yPos,res) {
       "습도"+wetPercent+"기후"+skyForm+"온도"+temperature+"최저온도"+minTemp);
     
       var weatherImg;
-      var tempStyle;    //cloth style 
       
       if(skyForm == 0)
         weatherImg = 'sun.png';
@@ -86,8 +85,34 @@ exports.realTimeWeather = function(xPos,yPos,res) {
         weatherImg = 'lotsofclound.png';
       else
         weatherImg = 'heurim.png';
-        
-      //tempStyle = checkStyle(temperature);
+    
+      var userStyle;
+      switch(checkStyle(temperature)){
+          case 0:
+              userStyle = "오늘 날씨는 매우 더운 여름 날씨입니다.짧은 반바지나 민소매 등 얇게 입는 것을 권합니다."; 
+              break;
+          case 1:
+              userStyle = "오늘 날씨는 저녁의 여름 날씨입니다.적당한 반팔이나 얇은 긴팔 등을 입는 것을 권합니다."; 
+              break;
+          case 2:
+              userStyle = "오늘 날씨는 적당한 여름 날씨입니다.긴팔티 혹은 면바지 등을 입는 것을 권합니다."; 
+              break;
+          case 3:
+              userStyle = "오늘 날씨는 더운 봄 날씨입니다.슬슬 두꺼운 옷을 입는 것도 괜찮습니다."; 
+              break;
+          case 4:
+              userStyle = "오늘 날씨는 쌀쌀한 수준입니다.얇은 외투를 입어 따뜻한 온도를 유지하기를 권합니다."; 
+              break;
+          case 5:
+              userStyle = "오늘 날씨는 추운 가을 날씨입니다.트렌치 코트, 따뜻한 니트 등을 입는 것을 권합니다."; 
+              break;
+          case 6:
+              userStyle = "오늘 날씨는 초겨울 날씨입니다.코트, 가죽 자켓 등으로 추위를 이겨내시길 권합니다."; 
+              break;
+          case 7:
+              userStyle = "오늘 날씨는 추운 겨울 날씨입니다.목도리, 패딩 등을 입는 것을 권합니다."; 
+              break;
+      }
         
       res.render('index', {
         temp: temperature,
@@ -95,9 +120,29 @@ exports.realTimeWeather = function(xPos,yPos,res) {
         rainForm: rainForm,
         wetPercent: wetPercent,
         minTemp: minTemp,
-        weatherImg : weatherImg
+        weatherImg : weatherImg,
+        userStyle: userStyle
       });
     });
    
 }
 
+function checkStyle(temperature){
+    
+    if(temperature >= 27)
+        return 0
+    else if(temperature >= 23)
+        return 1
+    else if(temperature >= 20)
+        return 2
+    else if(temperature >= 17)
+        return 3
+    else if(temperature >= 12)
+        return 4
+    else if(temperature >= 10)
+        return 5
+    else if(temperature >= 6)
+        return 6
+    else
+        return 7
+}
