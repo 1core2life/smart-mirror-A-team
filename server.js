87d6cc577450c532c603c28aea22de3163bb0db7
최재혁 <0206cjh@daum.net>
2017-09-03 08:17:32 +0000

    sujung

--- server.js
+++ server.js
@@ -66,7 +66,9 @@ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
     });
  router.post('/music', function(req, res){
         var musicName = req.body.musicname;
+	 console.log(musicName);
 	    music.searchMusic(res,musicName);
+	 	
     });  
  router.post('/news', function(req, res){
 	       news.news_result(res);
